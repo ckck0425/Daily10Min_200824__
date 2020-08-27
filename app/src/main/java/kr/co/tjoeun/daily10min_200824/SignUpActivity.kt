1fd604945993e64c6e38ec31da3336a8db30e155
yugeong ko <ckck0425ko@gmail.com>
2020-08-27 19:30:57 +0900

    회원가입 버튼 누르면 => 가입해도 되는지 검사. 회원가입 기능 완성 (서버에 가입 요청 => 응답에 따른 UI 처리)

--- app/src/main/java/kr/co/tjoeun/daily10min_200824/SignUpActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10min_200824/SignUpActivity.kt
@@ -1,15 +1,22 @@
 package kr.co.tjoeun.daily10min_200824
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kr.co.tjoeun.daily10min_200824.utils.ServerUtil
 import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
+
+//    아이디 중복검사 통과 여부
+    var isIdOk = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +31,74 @@ class SignUpActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-        signUpPwEdt.addTextChangedListener(object : TextWatcher{
+        signUpBtn.setOnClickListener {
+
+//            아이디를 사용해도 되는지? (중복검사를 통과했는지?)
+
+            if (!isIdOk) {
+
+//                사용하면 안되는 경우 => 회원가입 이벤트 강제 종료
+                Toast.makeText(mContext, "아이디 중복검사를 통과해야 합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+//            비밀번호를 써도 되는지?
+            if (signUpPasswordEdt.text.length < 8) {
+                Toast.makeText(mContext, "비밀번호는 8글자 이상이어야 합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+//            사용해도 된다면 다음 로직 진행
+//            닉네임은 한번 정하면 변경이 불가능합니다. 정말 회원가입 하시겠습니까?
+
+            val alert = AlertDialog.Builder(mContext)
+            alert.setTitle("회원가입 안내")
+            alert.setMessage("닉네임은 한번 정하면 변경이 불가능합니다. 정말 회원가입 하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+
+                val inputId = signUpEmailEdt.text.toString()
+                val inputPw = signUpPasswordEdt.text.toString()
+                val inputNickName = nickNameEdt.text.toString()
+
+//                실제 회원가입 기능 (API) 호출
+                ServerUtil.putRequestSignUp(inputId, inputPw, inputNickName, object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
+
+//                        연습문제
+//                        1. 회원가입에 성공한 경우, "회원이 되신것을 환영합니다." 토스트 + 로그인화면 복귀
+//                        2. 회원가입에 실패한 경우, 왜 실패했는지 서버가 알려주는 메세지를 토스트로 출력
+
+                        val code = json.getInt("code")
+
+                        if (code == 200) {
+                            runOnUiThread {
+                                Toast.makeText(mContext, "회원이 되신것을 환영합니다.", Toast.LENGTH_SHORT).show()
+                                finish()
+                            }
+                        }
+                        else {
+
+                            val message = json.getString("message")
+                            runOnUiThread {
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+
+                })
+            })
+
+            alert.setNegativeButton("취소", null)
+            alert.show()
+        }
+
+//        비밀번호 입력칸의 내용 변경된 경우
+//        입력된 비번의 길이에 따른 문구 출력
+//        0글자 : "비밀번호를 입력해주세요."
+//        8글자 미만 : "비밀번호가 너무 짧습니다."
+//        8글자 이상 : "사용해도 좋은 비밀번호 입니다."
+
+        signUpPasswordEdt.addTextChangedListener(object : TextWatcher{
             override fun afterTextChanged(s: Editable?) {
             }
 
@@ -35,22 +109,15 @@ class SignUpActivity : BaseActivity() {
 
                 val inputPassword = s.toString()
 
-                if (s.length == 0 ) {
+                if (inputPassword.length == 0 ) {
                     passwordCheckResultTxt.text = "비밀번호를 입력해주세요."
                 }
-
                 else if ( inputPassword.length < 8){
                     passwordCheckResultTxt.text = "비밀번호가 너무 짧습니다."
-
                 }
-
                 else {
                     passwordCheckResultTxt.text = "사용해도 좋은 비밀번호입니다."
-
                 }
-
-                }
-
             }
         })
 
@@ -69,6 +136,7 @@ class SignUpActivity : BaseActivity() {
                 Log.d("입력문구", s.toString())
 
                 emailCheckResultTxt.text = "중복 확인을 해주세요."
+                isIdOk = false
 
             }
 
@@ -87,17 +155,14 @@ class SignUpActivity : BaseActivity() {
                     runOnUiThread {
                         if (code == 200) {
                         emailCheckResultTxt.text = "사용해도 좋은 이메일입니다."
+                            isIdOk = true
                     }
                     else {
                         emailCheckResultTxt.text = "중복된 이메일입니다."
+                            isIdOk = false
                     } }
-
-
-
                 }
             })
-
         }
-
     }
 }
