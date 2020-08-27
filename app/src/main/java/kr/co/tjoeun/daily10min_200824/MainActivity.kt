d11f929fa6e63bcb1cac9f8fcd1dee4bf0e21f0f
yugeong ko <ckck0425ko@gmail.com>
2020-08-27 21:04:17 +0900

    프로젝트 object 파싱해서> ArrayList 에 담아주기

--- app/src/main/java/kr/co/tjoeun/daily10min_200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10min_200824/MainActivity.kt
@@ -38,7 +38,14 @@ class MainActivity : BaseActivity() {
                 for (i in 0 until projectArr.length()){
                     val projectObj = projectArr.getJSONObject(i)
 
-                    Log.d("프로젝트 제목", projectObj.getString("title"))
+                    val project = Project()
+                    project.id = projectObj.getInt("id")
+                    project.title = projectObj.getString("title")
+                    project.imageUrl = projectObj.getString("img_url")
+                    project.description = projectObj.getString("description")
+
+                    mProjectList.add(project)
+
 
                 }
 
