ba8d9f9711a0907404e39e286e48e720dca5ecfb
yugeong ko <ckck0425ko@gmail.com>
2020-08-27 19:42:46 +0900

    자동로그인 체크 여부 저장 / 조회 기능 추가

--- app/src/main/java/kr/co/tjoeun/daily10min_200824/adapters/ProjectAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10min_200824/adapters/ProjectAdapter.kt
@@ -1,4 +1,24 @@
 package kr.co.tjoeun.daily10min_200824.adapters
 
-class ProjectAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.co.tjoeun.daily10min_200824.datas.Project
+
+class ProjectAdapter(
+    val mContext: Context,
+     resId: Int,
+    val mList : List<Project>) : ArrayAdapter<Project>(mContext, resId, mList){
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+
+
+    }
+
+
 }
