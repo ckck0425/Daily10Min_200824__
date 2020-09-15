ab1af64350229b441ca07f094684bb62890da22f
yugyeong ko <ckck0425ko@gmail.com>
2020-09-15 21:36:12 +0900

    어떤 프로젝트의 멤버들을 보는지 전달

--- app/src/main/java/kr/co/tjoeun/daily10min_200824/ViewProjectMembersActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10min_200824/ViewProjectMembersActivity.kt
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.co.tjoeun.daily10min_200824.BaseActivity
 import kr.co.tjoeun.daily10min_200824.R
+import kr.co.tjoeun.daily10min_200824.datas.Project
+import kr.co.tjoeun.daily10min_200824.datas.User
 
 class ViewProjectMembersActivity : BaseActivity() {
+
+//    프로젝트 상세화면에서 넘겨준, 프로젝트 데이터를 담기 위한 멤버변수
+
+    lateinit var mProject : Project
+    val mProjectMembers = ArrayList<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -23,6 +30,13 @@ class ViewProjectMembersActivity : BaseActivity() {
 
     override fun setValues() {
 
+        mProject = intent.getSerializableExtra("project") as Project
+        getProjectMembersFromServer()
+    }
+
+//    서버에서 프로젝트의 참여 멤버 불러오는 기능
+
+    fun getProjectMembersFromServer() {
 
     }
 
