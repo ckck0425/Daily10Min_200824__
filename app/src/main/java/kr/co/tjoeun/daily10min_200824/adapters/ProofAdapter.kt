0f779815cf5736617f6d6aa9b82d402aef597b16
yugyeong ko <ckck0425ko@gmail.com>
2020-09-16 22:03:54 +0900

    인증글 이미지 처리

--- app/src/main/java/kr/co/tjoeun/daily10min_200824/adapters/ProofAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10min_200824/adapters/ProofAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import kr.co.tjoeun.daily10min_200824.R
 import kr.co.tjoeun.daily10min_200824.datas.Proof
 
@@ -30,6 +31,32 @@ class ProofAdapter {
             val date = mList[position]
 
             proofContentTxt.text = data.content
+
+//        첨부된 이미지가 있다면 => 이미지뷰를 화면에 표시
+
+
+
+//        인증글에 사진이 아예 없다면 => 이미지뷰 자체를 숨김처리
+
+
+
+            if (data.imgList.size == 0) {
+
+//            사진이 없는 글
+                proofFirstImg.visibility = View.GONE
+            }
+
+
+
+            else {
+
+//            하나 이상의 사진이 있는 경우
+
+            proofFirstImg.visibility = View.VISIBLE
+               Glide.with(mContext).load(data.imgList[0]).into(proofFirstImg)
+
+            }
+
             return row
 
         }
